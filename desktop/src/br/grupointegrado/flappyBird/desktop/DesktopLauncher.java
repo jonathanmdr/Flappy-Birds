ee866af8bd0a262bbb2afc015c2ab58b45e5b277
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2015-10-05 21:12:12 -0300

    Criação da Classe Util, Passaro, feito o chão do jogo e o corpo do Pássaro.

--- desktop/src/br/grupointegrado/flappyBird/desktop/DesktopLauncher.java
+++ desktop/src/br/grupointegrado/flappyBird/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import br.grupointegrado.flappyBird.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 600;
-		config.height = 480;
+		config.width = 480;
+		config.height = 640;
 		new LwjglApplication(new MainGame(), config);
 	}
 }
