97f012812c9b73031c7b028f53f7afc408f0aaf0
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2015-10-19 20:56:33 -0200

    Adicionar arquivos assets, classe bodyeditorloader, arquivo json, impulso linear no passaro.

--- core/src/br/grupointegrado/flappyBird/Passaro.java
+++ core/src/br/grupointegrado/flappyBird/Passaro.java
@@ -1,11 +1,13 @@
 package br.grupointegrado.flappyBird;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 /**
@@ -33,11 +35,38 @@ public class Passaro {
 
         corpo = Util.criarCorpo(mundo, BodyDef.BodyType.DynamicBody, x, y);
 
-        CircleShape shape = new CircleShape();
-        shape.setRadius(18 / Util.PIXEL_METRO);
+        FixtureDef definicao = new FixtureDef();
+        definicao.density = 1;
+        definicao.friction = 0.4f;
+        definicao.restitution = 0.3f;
 
-        Fixture forma = Util.criarForma(corpo, shape, "PASSARO");
-        shape.dispose();
+        BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("physics/bird.json"));
+        loader.attachFixture(corpo, "bird", definicao, 1, "PASSARO");
+
+    }
+
+    /**
+     * Atualiza o comportamento do passaro
+     * @param delta
+     */
+    public void atualizar(float delta){
+        atualizarVelocidade();
+    }
+
+    private void atualizarVelocidade() {
+        corpo.setLinearVelocity(2f, corpo.getLinearVelocity().y);
+    }
+
+    /**
+     * Aplica uma força positiva no Y para simular o pulo
+     */
+    public void pular(){
+        corpo.setLinearVelocity(corpo.getLinearVelocity().x, 0);
+        corpo.applyForceToCenter(0, 100, false);
+    }
+
+    public Body getCorpo(){
+        return corpo;
     }
 
 
