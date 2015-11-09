4457bf181158060c5087fb51d2c76e7827dccae4
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2015-11-09 21:09:37 -0200

    Criação de Todos Obstáculos durante o Jogo, Fontes, Estilos, Pontuação do jogo.

--- core/src/br/grupointegrado/flappyBird/Obstaculo.java
+++ core/src/br/grupointegrado/flappyBird/Obstaculo.java
@@ -56,11 +56,11 @@ public class Obstaculo {
         largura = 40 / Util.PIXEL_METRO;
         altura = camera.viewportHeight / Util.PIXEL_METRO;
 
-        float xInicial = largura;
+        float xInicial = largura + (camera.viewportWidth / 2 / Util.PIXEL_METRO);
         if (ultimoObstaculo != null){
             xInicial = ultimoObstaculo.getPosX();
         }
-        posX = xInicial + 8; //8 é o espaço entre os obstaculos <---->
+        posX = xInicial + 4; //4 é o espaço entre os obstaculos <---->
 
         // parcela é o tamanho da tela dividido por 6,
         // para encontrar a posição Y dos obstáculos
@@ -81,4 +81,31 @@ public class Obstaculo {
         mundo.destroyBody(corpoBaixo);
     }
 
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getLargura() {
+        return largura;
+    }
+
+    public void setLargura(float largura) {
+        this.largura = largura;
+    }
+
+    public void setPosX(float posX) {
+        this.posX = posX;
+    }
+
+    public boolean isPassou() {
+        return passou;
+    }
+
+    public void setPassou(boolean passou) {
+        this.passou = passou;
+    }
 }
--- core/src/br/grupointegrado/flappyBird/TelaJogo.java
+++ core/src/br/grupointegrado/flappyBird/TelaJogo.java
@@ -1,15 +1,27 @@
 package br.grupointegrado.flappyBird;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 
 /**
  * Created by jonat_000 on 28/09/2015.
@@ -20,7 +32,15 @@ public class TelaJogo extends TelaBase {
     private World mundo; // representa o mundo do Box2D
     private Body chao; // corpo do chão no jogo
     private Passaro passaro;
+    private Array<Obstaculo> obstaculos = new Array<Obstaculo>();
 
+    private int pontuacao = 0;
+    private BitmapFont fontePontuacao;
+    private Stage palcoInformacoes;
+    private Label lbPontuacao;
+    private ImageButton btnPlay;
+    private ImageButton btnGameOver;
+    private OrthographicCamera cameraInfo;
 
     private Box2DDebugRenderer debug; //Desenha o mundo na tela para ajudar no desenvolvimento
 
@@ -33,13 +53,74 @@ public class TelaJogo extends TelaBase {
     @Override
     public void show() {
         camera = new OrthographicCamera(Gdx.graphics.getWidth() / Util.ESCALA, Gdx.graphics.getHeight() / Util.ESCALA);
+        cameraInfo = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         debug = new Box2DDebugRenderer();
         mundo = new World(new Vector2(0, -9.8f), false);
+        mundo.setContactListener(new ContactListener() {
+            @Override
+            public void beginContact(Contact contact) {
+                detectarColisao(contact.getFixtureA(), contact.getFixtureB());
+            }
+
+            @Override
+            public void endContact(Contact contact) {
+
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold) {
+
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse) {
+
+            }
+        });
 
         initChao();
         initPassaro();
+        initFontes();
+        initInformacoes();
+    }
 
-        new Obstaculo(mundo, camera, null);
+    private boolean gameOver = false;
+
+    /**
+     * verifica se o passaro esta envolvido na colisão
+     * @param fixtureA
+     * @param fixtureB
+     */
+    private void detectarColisao(Fixture fixtureA, Fixture fixtureB) {
+        if ("PASSARO".equals(fixtureA.getUserData()) || "PASSARO".equals(fixtureB.getUserData())){
+            //game over
+            gameOver = true;
+        }
+    }
+
+    private void initFontes() {
+        FreeTypeFontGenerator.FreeTypeFontParameter fonteParam = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        fonteParam.size = 56;
+        fonteParam.color = Color.WHITE;
+        fonteParam.shadowColor = Color.BLACK;
+        fonteParam.shadowOffsetX = 4;
+        fonteParam.shadowOffsetY = 4;
+
+        FreeTypeFontGenerator gerador = new FreeTypeFontGenerator(Gdx.files.internal("fonts/roboto.ttf"));
+        fontePontuacao = gerador.generateFont(fonteParam);
+        gerador.dispose();
+    }
+
+    private void initInformacoes() {
+        palcoInformacoes = new Stage(new FillViewport(cameraInfo.viewportWidth, cameraInfo.viewportHeight, cameraInfo));
+        Gdx.input.setInputProcessor(palcoInformacoes);
+
+        Label.LabelStyle estilo = new Label.LabelStyle();
+        estilo.font = fontePontuacao;
+
+        lbPontuacao = new Label("0", estilo);
+        palcoInformacoes.addActor(lbPontuacao);
     }
 
     private void initChao() {
@@ -78,6 +159,7 @@ public class TelaJogo extends TelaBase {
      * @param delta
      */
     private void renderizar(float delta) {
+        palcoInformacoes.draw();
     }
 
     /**
@@ -86,12 +168,53 @@ public class TelaJogo extends TelaBase {
      * @param delta
      */
     private void atualizar(float delta) {
+        palcoInformacoes.act(delta);
+        
         passaro.atualizar(delta);
         mundo.step(1f / 60f, 6, 2);
+
+        atualizarInformacoes();
+        atualizarObstaculos();
         atualizarCamera();
         atualizarChao();
+
         if(pulando){
             passaro.pular();
+        }
+    }
+
+    private void atualizarInformacoes() {
+        lbPontuacao.setText(pontuacao + "");
+        lbPontuacao.setPosition(cameraInfo.viewportWidth / 2 - lbPontuacao.getPrefWidth() / 2,
+                                cameraInfo.viewportHeight - lbPontuacao.getPrefHeight());
+    }
+
+    private void atualizarObstaculos() {
+        //enquanto a lista tiver menos que 4 obstaculos, crie um novo obstaculo;
+        while(obstaculos.size < 4){
+            Obstaculo ultimo = null;
+            if (obstaculos.size > 0){
+                ultimo = obstaculos.peek(); //comando .peek() recupera o ultimo item da lista
+            }
+            Obstaculo o = new Obstaculo(mundo, camera, ultimo);
+            obstaculos.add(o);
+        }
+        //verifica se os obstaculos sariam da tela para removê-los
+        for (Obstaculo o : obstaculos){
+            //calcula a lateral inicial da camera
+            float inicioCamera = passaro.getCorpo().getPosition().x - (camera.viewportWidth / 2 / Util.PIXEL_METRO) - o.getLargura();
+            //verifica se o obstaculo saiu da tela
+            if(inicioCamera > o.getPosX()){
+                o.remover();
+                obstaculos.removeValue(o, true);
+            }
+            else if(!o.isPassou() && o.getPosX() < passaro.getCorpo().getPosition().x){
+                o.setPassou(true);
+
+                //calcular pontuação
+                pontuacao ++;
+                //reproduzir os sons do jogo
+            }
         }
     }
 
@@ -113,6 +236,8 @@ public class TelaJogo extends TelaBase {
         camera.setToOrtho(false, width / Util.ESCALA, height / Util.ESCALA);
         camera.update();
         redimensionaChao();
+        cameraInfo.setToOrtho(false, width, height);
+        cameraInfo.update();
     }
 
     /**
@@ -148,5 +273,7 @@ public class TelaJogo extends TelaBase {
     public void dispose() {
         debug.dispose();
         mundo.dispose();
+        palcoInformacoes.dispose();
+        fontePontuacao.dispose();
     }
 }
